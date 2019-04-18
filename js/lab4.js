7df38cdb2593005f02f47439cff4de746d685713
Richard <r.mora@students.clark.edu>
2019-04-18 00:19:17 -0700

    lab no 4 complete

--- js/lab4.js
+++ js/lab4.js
@@ -3,36 +3,88 @@
 
 function verse1(){
     let output = '';
-    // your code here
+    output = 'You are a brick tied to me that\'s dragging me down <br>Strike a match and I\'ll burn you to the ground <br>We are the jack-o-lanterns in July setting fire to the sky <br>Here, here comes this rising tide, so come on';
 
     return output;
 }
 
 function verse2(){
     let output = '';
-    // your code here
+    output = 'Cross walks and crossed hearts and hope-to-dies<br> Silver clouds with grey linings';
+
+    return output;
+}
+
+function verse3(){
+    let output = '';
+    output ='Bring home the boys and scrap scrap metal the tanks<br> Get hitched, make a career out of robbing banks<br> Because the world is just a teller and we are wearing black masks<br> "You broke our spirit," says the note we pass';
 
     return output;
 }
 
 function chorus1(){
     let output = '';
-    // your code here
+    output = 'Put on your war paint';
 
     return output;
 }
 
 function chorus2(){
     let output = '';
-    // your code here
+    output = 'So we can take the world back from a heart attack<br> One maniac at a time we will take it back<br> You know time crawls on when you\'re waiting for the song to start<br> So dance alone to the beat of your heart';
+
+    return output;
+}
+
+function chorus3(){
+    let output = '';
+    output = 'Hey young blood, doesn\'t it feel like our time is running out?<br> I\'m gonna change you like a remix<br> Then I\'ll raise you like a phoenix<br> Wearing our vintage misery<br> No, I think it looked a little better on me<br> I\'m gonna change you like a remix<br> Then I\'ll raise you like a phoenix';
+
+    return output;
+}
+
+function chorus3_2(){
+    let output = '';
+    output = 'Hey young blood, doesn\'t it feel like our time is running out?<br> I\'m gonna change you like a remix<br> Then I\'ll raise you like a phoenix';
+
+    return output;
+}
+
+function chorus4(){
+    let output = '';
+    output = 'The war is won, before it\'s begun<br> Release the doves, surrender love';
+
+    return output;
+}
+
+function chorus4_2(){
+    let output = '';
+    output = 'The war is won, before it\'s begun (Wave the white flag!)<br> Release the doves, surrender love (Wave the white flag!)';
 
     return output;
 }
 
 
-
 function main(){
     let finalOutput = ''; // used to build lyrics string
+    finalOutput = `<h2>Phoenix by Fall Out Boy</h2><br>
+                   <p>${chorus1()}</p> 
+                   <p>${verse1()}</p> 
+                   <p>${chorus1()}</p> 
+                   <p>${verse2()}</p> 
+                   <p>${chorus2()}</p> 
+                   <p>${chorus3()}</p> 
+                   <p>${verse3()}</p> 
+                   <p>${chorus2()}</p> 
+                   <p>${chorus3()}</p> 
+                   <p>${chorus1()}</p> 
+                   <p>${chorus4()}</p> 
+                   <p>${chorus4()}</p> 
+                   <p>${chorus4()}</p> 
+                   <p>${chorus4_2()}</p> 
+                   <p>${chorus3()}</p> 
+                   <p>${chorus3_2()}</p> 
+                   <p>${chorus1()}</p>`;
     // call verse and chorus functions
     // each verse/chorus should return text including HTML paragraph tags
 
